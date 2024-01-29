4b42297695d24577dd73d927fee62669fbd6a0d5
Jerry Tarus <jerrytarus@gmail.com>
2024-01-29 17:05:40 +0300

    Added validation for vendor sweet price attribute

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -20,4 +21,23 @@ class Sweet(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
     vendor_sweets = db.relationship('VendorSweet', backref='sweet', lazy=True)
+
+class VendorSweet(db.Model):
+    __tablename__ = 'vendor_sweets'
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Float, nullable=False)
+    sweets_id = db.Column(db.Integer, db.ForeignKey('sweet.id'), nullable=False)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendor.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if not value:
+            raise ValueError('Price cannot be blank')
+        if value < 0:
+            raise ValueError('Price cannot be a negative number')
+        return value
+
+
 
