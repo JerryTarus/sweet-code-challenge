d714314e920e5390c67648f20d76be13fe32c67f
Jerry Tarus <jerrytarus@gmail.com>
2024-01-29 16:31:05 +0300

    Added Sweet model

--- app/models.py
+++ app/models.py
@@ -12,3 +12,12 @@ class Vendor(db.Model):
     vendor_sweets = db.relationship('VendorSweet', backref='vendor', lazy=True)
 
 # add any models you may need. 
+    
+class Sweet(db.Model):
+    __tablename__ = 'sweet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, server_default=db.func.now(), onupdate=datetime.utcnow)
+    vendor_sweets = db.relationship('VendorSweet', backref='sweet', lazy=True)
+
